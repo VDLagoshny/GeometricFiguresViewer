c16e1280e90fbaffd2c87c8757cc1857aefa7496
VDLagoshny <botafogos@yandex.ru>
2024-02-16 14:49:52 +0300

    Add DrawService

---
+++ GeometricFiguresViewer/DrawService/DrawService.cs
@@ -0,0 +1,26 @@
+﻿namespace GeometricFiguresViewer.DrawService
+{
+    /// <summary>
+    ///  Класс сервиса отрисовки фигуры
+    /// </summary>
+    internal sealed class DrawService: IDrawService
+    {
+        /// <summary>
+        /// Метод отрисовки фигуры
+        /// </summary>
+        /// <param name="chars">Массив символов, отображающих 
+        /// фигуру на консоли, тип char[]</param>
+        public void Draw(char[] chars)
+        {
+            if (chars.Length == 0)
+                throw new ArgumentException("Output array is empty");
+
+            foreach (var pixel in chars)
+            {
+                Console.Write(pixel);
+            }
+
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
---
+++ GeometricFiguresViewer/DrawService/IDrawService.cs
@@ -0,0 +1,15 @@
+﻿namespace GeometricFiguresViewer.DrawService
+{
+    /// <summary>
+    ///  Интерфейс сервиса отрисовки фигуры
+    /// </summary>
+    internal interface IDrawService
+    {
+        /// <summary>
+        /// Метод отрисовки фигуры
+        /// </summary>
+        /// <param name="chars">Массив символов, отображающих 
+        /// фигуру на консоли, тип char[]</param>
+        void Draw(char[] chars);
+    }
+}
